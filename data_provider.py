8bab3bd6f7be5058d2805d1d75f85a6d343693b0
Mikron <mlabedowicz@future-processing.com>
2015-10-19 12:48:29 +0200

    Working DB system

--- data_provider.py
+++ data_provider.py
@@ -11,7 +11,8 @@ def create_connection():
 def connect():
     db = getattr(g, '_database', None)
     if db is None:
-        db = g._database = create_connection()
+        db = g._database = create_connection()        
+        db.row_factory = make_dictionaries
     return db
 
 @app.teardown_appcontext
@@ -20,6 +21,15 @@ def disconnect(exception):
     if db is not None:
         db.close()
         
+def make_dictionaries(cursor, row):
+    return dict((cursor.description[idx][0], value)
+        for idx, value in enumerate(row))
+
 def get_list_of_recommendations():
     sql = 'SELECT title, description, season_count, episodes_per_season, episode_length, trailer, person, associated_links FROM recommendation'
-    return connect().execute(sql)
+        
+    cursors = connect().execute(sql)    
+    rows = cursors.fetchall()
+    cursors.close()
+    
+    return rows
